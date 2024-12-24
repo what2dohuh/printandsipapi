7d9d97f4697eead6e563e3a82275f69f89d8d6b9
Deep Sharma <deepshar17@gmail.com>
2024-12-24 18:11:00 +0530

    first commit

--- server.js
+++ server.js
@@ -28,7 +28,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "printandsips.shop",
+    origin:  [
+      "https://printandsips.shop",
+      "https://printandsip.onrender.com",
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
